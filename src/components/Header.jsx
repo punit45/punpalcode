0c4956dd03f0f5ec1814fe76f50981daf4c63de4
punit <73206636+punit45@users.noreply.github.com>
2024-12-06 07:18:09 +0530

    product part done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import Help from "../assets/message-circle-question.png";
+import Search from "../assets/search.png";
+import Atom from "../assets/atom.png";
+import Bell from "../assets/bell-dot.png";
+import Downn from "../assets/chevron-down.png";
+import Tinu from "../assets/tinu.jpg";
 
 const Header = () => {
   return (
-    <div>
-       <header className="bg-blue-600 text-white p-4 flex items-center">
-      <h1 className="text-xl">Header</h1>
-    </header>
+    <div className='flex flex-row items-center justify-between mx-4 mt-2 mb-8 bg-gray-800 border-none rounded-2xl h-20'>
+      <div>
+        <h2 className='text-white font-semibold px-4 items-center justify-center text-2xl'>Design Studio</h2>
+      </div>
+      <div className='flex flex-row items-center justify-evenly mr-16 gap-4'>
+        <div className='flex text-white font-normal text-sm border gap-2 border-blue-400 rounded-xl p-2'>
+          <img src={Help} alt='' />
+          Support Request
+        </div>
+        <div className='flex text-white font-normal text-sm border gap-2 border-blue-400 rounded-xl p-2'>
+          <img src={Atom} alt='' />
+          Product Tour
+        </div>
+        <div className='flex flex-row border border-gray-500 rounded-lg items-center justify-around gap-2 p-1'>
+          <input className='border-none outline-none rounded-lg text-left px-1 text-gray-500' type="text" placeholder='Search Project' />
+          <div className='border border-gray-500 p-1 rounded-lg flex items-center justify-center cursor-pointer'>
+            <img src={Search} alt="Search Icon" className='w-6 h-6' />
+          </div>
+        </div>
+        <div className='border border-gray-500 p-1 rounded-lg flex items-center justify-center cursor-pointer'>
+            <img src={Bell} alt="Search Icon" className='w-6 h-6' />
+          </div>
+          <div className='border-gray-700 flex items-center justify-between gap-2 cursor-pointer'>
+            <img src={Tinu} alt="Search Icon" className='w-10 h-10 rounded-3xl cursor-pointer' />
+            <h2 className='text-white font-medium'>Punit Panda</h2>
+            <img src={Downn} alt="" className='text-white' />
+          </div>
+      </div>
     </div>
   )
 }
--- src/components/ProductDisplay.jsx
+++ src/components/ProductDisplay.jsx
@@ -1,13 +1,119 @@
-import React from 'react'
+import React from 'react';
+import Youtube from "../assets/youtube.png";
+import Dot from "../assets/ellipsis.png";
+import Load from "../assets/refresh-cw.png";
+import Square from "../assets/square.png";
+import Circle from "../assets/circle.png";
+import { IoLogoYoutube } from "react-icons/io";
+
 
 const ProductDisplay = () => {
+  const backgroundImages = [
+    'https://media.istockphoto.com/id/1321486723/photo/japanese-man-spending-weekend-morning-painting-in-his-bedroom-at-home.jpg?s=612x612&w=0&k=20&c=vQNWXbNJ6xteAa33XmgBY4Uow0blGgCSl8gvRxuxSi4=',
+    'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Defense-and-intelligence-Hero-Image-640x480?resMode=sharp2&op_usm=1.5,0.65,15,0&wid=700&hei=540&qlt=100&fmt=png-alpha&fit=constrain',
+    'https://hasonss.com/blogs/wp-content/uploads/2023/10/types-of-technology-2.webp',
+    'https://static.vecteezy.com/system/resources/previews/024/669/489/non_2x/mountain-countryside-landscape-at-sunset-dramatic-sky-over-a-distant-valley-green-fields-and-trees-on-hill-beautiful-natural-landscapes-of-the-carpathians-generative-ai-variation-5-photo.jpeg',
+    'https://admin.stayatpurijagannatha.in/images/frontend/main-slider-1_1670308972.jpg',
+    'https://storage.googleapis.com/redbrics-prod-backend.appspot.com/blog/title/_1685700473338_10-Historical-Monuments-Of-India--Our-Countrys-Majestic-Tryst-With-Architecture.webp'
+  ];
+  const playlistNames = [
+    "Artist Playlist",
+    "Defense Playlist",
+    "Technology Playlist",
+    "Nature Playlist",
+    "Temples Playlist",
+    "Monument Playlist",
+
+  ];
+
   return (
- 
-      <div className="flex-1 p-4 bg-gray-100">
-      <h2>Product Display</h2>
-      <p>Product information here...</p>
+    <div className="flex-1 p-6 bg-gray-900 border-none outline-none rounded-3xl m-2 h-auto">
+      <h2 className='text-white font-medium text-xl mb-4 text-start'>Product Playlists</h2>
+      <div className='flex flex-wrap gap-6 border-gray-700 bg-gray-700 p-4 w-full h-auto'>
+        {/* cards */}
+        <div className="flex flex-wrap gap-4">
+          {[...Array(6)].map((_, index) => (
+            <div
+              key={index}
+              className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-between"
+              style={{
+                backgroundImage: `url(${backgroundImages[index]})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '350px' // Adjust the height to be more appropriate
+              }}
+            >
+              <div className="flex justify-end px-4 pt-4">
+                <button
+                  id="dropdownButton"
+                  data-dropdown-toggle="dropdown"
+                  className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+                  type="button"
+                >
+                  <span className="sr-only">Open dropdown</span>
+                  <svg
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 16 3"
+                  >
+                    <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                  </svg>
+                </button>
+                {/* Dropdown menu */}
+                <div
+                  id="dropdown"
+                  className="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                >
+                  <ul className="py-2" aria-labelledby="dropdownButton">
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Edit
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Export Data
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Delete
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center pb-1">
+                <h5 className="text-xl font-medium text-white mt-40 dark:text-white">
+                  {playlistNames[index]}
+                </h5>
+                <div className="flex w-full md:mt-6 gap-2 mt-auto">
+                  <a
+                    href="#"
+                    className="inline-flex gap-4 items-center w-full px-6 mb-[-4px] justify-center py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    <img className='' src={Youtube} alt='' />5 Videos
+
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-   
   )
 }
 
